e644837b47e50812e7226f6f45f573263053e57d
paulobernardoaf <pbaf@ic.ufal.br>
2022-08-23 23:31:01 -0300

    add card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import * as Styled from './styles';
+
+const Card = ({ children }) => {
+  return <Styled.Wrapper>{children}</Styled.Wrapper>;
+};
+
+export default Card;
---
+++ src/components/Card/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  background: var(--white);
+  box-shadow: 0px 3px 10px #c4c8d08f;
+  border-radius: 20px;
+  padding: 20px;
+`;
